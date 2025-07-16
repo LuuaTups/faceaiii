dba3132f8d7f07584687156bfed7b64b9a199ce1
LuuaTups <63587928+LuuaTups@users.noreply.github.com>
2025-07-16 15:45:38 +0300

    Fix Metro importLocationsPlugin version mismatch

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,4 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
+// metro.config.js
+const { getDefaultConfig } = require('@expo/metro-config');
 const config = getDefaultConfig(__dirname);
-
 module.exports = config;
